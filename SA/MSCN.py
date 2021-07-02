b972aa15f0249cf4d74961f2b2f9557d1eb9a575
sharycao <54173538+sharycao@users.noreply.github.com>
2021-07-02 18:31:06 +0800

    Add files via upload

---
+++ SA/MSCN.py
@@ -0,0 +1,129 @@
+import numpy as np
+import os
+import keras
+from keras.layers import Conv3D,  Deconvolution3D, Flatten, Dense, add, BatchNormalization
+from keras.layers import Input,  Activation, Concatenate, Dropout, MaxPooling3D, Reshape
+from keras.models import Model
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, cohen_kappa_score
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.optimizers import Adam, SGD
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+#from keras import backend as K
+#K.set_image_dim_ordering('th')
+from operator import truediv
+import spectral
+from keras.utils import np_utils
+import time
+from plotly.offline import init_notebook_mode
+init_notebook_mode(connected=True)
+PATH = os.getcwd()
+print(PATH)
+
+for i in range(1, 11):
+    windowSize = 9
+    numPCAcomponents = 20
+    testRatio = 0.99
+
+    #X_train = np.load("./dataset/XtrainWindowSize" + str(windowSize) + "testRatio" + str(testRatio) + ".npy")
+    #y_train = np.load("./dataset/ytrainWindowSize" + str(windowSize) + "testRatio" + str(testRatio) + ".npy")
+    X_train = np.load("./dataset/XtrainWindowSize" + str(windowSize) + "PCA" + str(numPCAcomponents) + "testRatio" + str(testRatio) + ".npy")
+    y_train = np.load("./dataset/ytrainWindowSize" + str(windowSize) + "PCA" + str(numPCAcomponents) + "testRatio" + str(testRatio) + ".npy")
+
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], X_train.shape[2], X_train.shape[3], 1))
+    y_train = np_utils.to_categorical(y_train)
+
+
+    input_layer = Input(shape=(9, 9, 20, 1), name='Pavia_Input')
+
+
+
+#第一层
+    #光谱
+    conv_layer11 = Conv3D(filters=8, kernel_size=(1, 1, 3),  padding='same')(input_layer)
+    BN11 = BatchNormalization()(conv_layer11)
+    relu11 = Activation('relu')(BN11)
+
+    pool1 = MaxPooling3D(pool_size=(3, 3, 1), strides=(1, 1, 1), padding='same')(relu11)
+
+    conv_layer12 = Conv3D(filters=8, kernel_size=(3, 3, 1), padding='same')(pool1)
+    BN12 = BatchNormalization()(conv_layer12)
+    relu12 = Activation('relu')(BN12)
+
+    conv_layer13 = Conv3D(filters=16, kernel_size=(3, 3, 20))(relu12)
+    conv3d_shape13 = conv_layer13._keras_shape
+    cl13 = Reshape((conv3d_shape13[1], conv3d_shape13[2], conv3d_shape13[3] * conv3d_shape13[4], 1))(conv_layer13)
+    BN13 = BatchNormalization()(cl13)
+    relu13 = Activation('relu')(BN13)
+
+
+
+
+    flatten_layer = Flatten()(relu13)
+
+
+#全连接层
+    #output_layer1 = GlobalAveragePooling3D()(flatten_layer)
+    Dense1 = Dense(256, activation='relu')(flatten_layer)
+    dropout1 = Dropout(rate=0.3)(Dense1)
+    Dense2 = Dense(128, activation='relu')(dropout1)
+    dropout2 = Dropout(rate=0.3)(Dense2)
+    output_layer = Dense(16, activation='softmax')(dropout2)
+
+    model = Model(inputs=[input_layer], outputs=[output_layer])
+    model.summary()
+
+#反向传播
+    SGD = keras.optimizers.SGD(lr=0.01)
+    #Adam = keras.optimizers.Adam(lr=0.0001)
+    model.compile(loss='categorical_crossentropy', optimizer=SGD, metrics=['accuracy'])
+
+
+    #检查
+
+    filepath = 'TKSN-9x9x20 - 1%' + "Window Size" + str(windowSize) + "num" + str(numPCAcomponents)\
+               + str(i) + '.hdf5'
+    #reduce_lr = ReduceLROnPlateau(monitor=*'val_acc', factor=0.5, patience=2, min_lr=0.0001, verbose=1)
+    checkpoint = ModelCheckpoint(filepath, monitor='accuracy', verbose=1, save_best_only=True, mode='max')
+    callbacks_list = [checkpoint]
+    start = time.time()
+    history = model.fit(x=X_train, y=y_train, verbose=1, batch_size=16, epochs=40,
+                        callbacks=callbacks_list)
+    end = time.time()
+    print(start - end)
+
+    #plt.plot(history.history['acc'])
+    #plt.plot(history.history['val_acc'])
+    #plt.title('model accuracy')
+    #plt.ylabel('accuracy')
+    #plt.xlabel('epoch')
+    #plt.legend(['train', 'test'], loc='upper left')
+    #plt.savefig(str(i)+'acc0.001'+".jpg")
+    #plt.show()
+
+
+    #plt.xlim((0, 30))
+
+    #plt.ylim((0, 1.6))
+    #my_x_ticks = np.arange(0, 30, 5)
+    #my_y_ticks = np.arange(0, 1.6, 0.15)
+    #plt.xticks(my_x_ticks)
+    #plt.yticks(my_y_ticks)
+    #plt.plot(history.history['loss'])
+    #plt.plot(history.history['val_loss'])
+    #plt.title('model loss')
+    #plt.ylabel('loss')
+    #plt.xlabel('epoch')
+    #plt.grid(True)
+    #plt.legend(['train', 'test'], loc='upper left')
+    #plt.savefig(str(i)+'loss0.01'+".jpg")
+    #plt.show()
+
+
+
+
+
+
+
+
+
---
+++ SA/dataset.py
@@ -0,0 +1,110 @@
+import numpy as np
+from sklearn.decomposition import PCA
+import scipy.io as sio
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+import os
+import random
+import scipy.ndimage
+from keras.utils import np_utils
+
+#读取
+def loadIndianPinesData():
+    data_path = os.path.join(os.getcwd(), 'data')
+    data = sio.loadmat(os.path.join(data_path, 'Salinas_corrected.mat'))['salinas_corrected']
+    labels = sio.loadmat(os.path.join(data_path, 'Salinas_gt.mat'))['salinas_gt']
+    return data, labels
+
+#划分训练集
+def splitTrainTestSet(X, y, testRatio, randomState=345):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testRatio, random_state=randomState,
+                                                        stratify=y)
+    return X_train, X_test, y_train, y_test
+
+#降维
+def applyPCA(X, numComponents=75):
+    newX = np.reshape(X, (-1, X.shape[2]))
+    pca = PCA(n_components=numComponents, whiten=True)
+    newX = pca.fit_transform(newX)
+    newX = np.reshape(newX, (X.shape[0], X.shape[1],numComponents))
+    return newX, pca
+
+#补零
+def padWithZeros(X, margin=2):
+    newX = np.zeros((X.shape[0] + 2 * margin, X.shape[1] + 2 * margin, X.shape[2]))
+    x_offset = margin
+    y_offset = margin
+    newX[x_offset:X.shape[0] + x_offset, y_offset:X.shape[1] + y_offset, :] = X
+    print(newX.shape)
+    return newX
+
+def createImageCubes(X, y, windowSize=5, removeZeroLabels = True):
+    margin = int((windowSize - 1) / 2)
+    zeroPaddedX = padWithZeros(X, margin=margin)
+    # split patches
+    patchesData = np.zeros((X.shape[0] * X.shape[1], windowSize, windowSize, X.shape[2]))
+    patchesLabels = np.zeros((X.shape[0] * X.shape[1]))
+    patchIndex = 0
+    for r in range(margin, zeroPaddedX.shape[0] - margin):
+        for c in range(margin, zeroPaddedX.shape[1] - margin):
+            patch = zeroPaddedX[r - margin:r + margin + 1, c - margin:c + margin + 1]
+            patchesData[patchIndex, :, :, :] = patch
+            patchesLabels[patchIndex] = y[r-margin, c-margin]
+            patchIndex = patchIndex + 1
+    if removeZeroLabels:
+        patchesData = patchesData[patchesLabels>0,:,:,:]
+        patchesLabels = patchesLabels[patchesLabels>0]
+        patchesLabels -= 1
+    return patchesData, patchesLabels
+
+#保存
+def savePreprocessedData(X_trainPatches, X_testPatches, y_trainPatches, y_testPatches, windowSize, wasPCAapplied = False, numPCAComponents = 0, testRatio = 0.9):
+    if wasPCAapplied:
+        with open( "./dataset/XtrainWindowSize" + str(windowSize) + "PCA" + str(numPCAComponents) + "testRatio" + str(testRatio) + ".npy", 'bw') as outfile:
+            np.save(outfile, X_trainPatches)
+        with open("./dataset/XtestWindowSize" + str(windowSize) + "PCA" + str(numPCAComponents) + "testRatio" + str(testRatio) + ".npy", 'bw') as outfile:
+            np.save(outfile, X_testPatches)
+        with open("./dataset/ytrainWindowSize" + str(windowSize) + "PCA" + str(numPCAComponents) + "testRatio" + str(testRatio) + ".npy", 'bw') as outfile:
+            np.save(outfile, y_trainPatches)
+        with open("./dataset/ytestWindowSize" + str(windowSize) + "PCA" + str(numPCAComponents) + "testRatio" + str(testRatio) + ".npy", 'bw') as outfile:
+            np.save(outfile, y_testPatches)
+    else:
+        with open("./dataset/XtrainWindowSize" + str(windowSize) + "testRatio" + str(testRatio) + ".npy", 'bw') as outfile:
+            np.save(outfile, X_trainPatches)
+        with open("./dataset/XtestWindowSize" + str(windowSize) + "testRatio" + str(testRatio) + ".npy", 'bw') as outfile:
+            np.save(outfile, X_testPatches)
+        with open("./dataset/ytrainWindowSize" + str(windowSize) + "testRatio" + str(testRatio) + ".npy", 'bw') as outfile:
+            np.save(outfile, y_trainPatches)
+        with open("./dataset/ytestWindowSize" + str(windowSize) + "testRatio" + str(testRatio) + ".npy", 'bw') as outfile:
+            np.save(outfile, y_testPatches)
+
+#正则化，多用于PCA前
+def Normalize(train_data):
+    """function to standardiza the data to zero mean and unit variance"""
+    mean, std = [], []
+    for i in range(train_data.shape[3]):
+        m = np.mean(train_data[:, :, :, i])
+        s = np.std(train_data[:, :, :, i])
+        mean.append(m)
+        std.append(s)
+
+    mean, std = np.array(mean), np.array(std)
+    x_train = (train_data - mean) / std
+    #x_test = (test_data - mean) / std
+    return x_train
+
+
+numComponents = 20
+testRatio = 0.99
+windowSize = 9
+margin = 4
+X, y = loadIndianPinesData()
+
+
+X1, pca = applyPCA(X, numComponents=numComponents)
+X2, y = createImageCubes(X1, y, windowSize=windowSize)
+
+X_train, X_test, y_train, y_test = splitTrainTestSet(X2, y, testRatio)
+
+savePreprocessedData(X_train, X_test, y_train, y_test, windowSize=windowSize, wasPCAapplied=True,
+                     numPCAComponents=numComponents, testRatio=testRatio)
